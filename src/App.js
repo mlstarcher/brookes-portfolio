37022bb9a464f66a3815fd7efb617f02fe5e2972
Matthew Starcher <mlstarcher@gmail.com>
2022-12-04 19:54:46 -0800

    Night 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import Header from './pages/Header.js';
 import Footer from './pages/Footer.js';
+import Home from './pages/Home.js';
+import ErrorPage from './pages/ErrorPage.js';
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <ErrorPage />
+  }
+]);
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <RouterProvider router={router} />
       <Footer />
     </div>
   );
