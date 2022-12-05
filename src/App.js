362ba5aa22de62695f25a51daff27bd0cf887c1e
Matthew Starcher <mlstarcher@gmail.com>
2022-12-04 16:39:19 -0800

    Created react app, configured tailwind

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl font-semibold">
+          Welcome to Brooke's Portfolio
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
