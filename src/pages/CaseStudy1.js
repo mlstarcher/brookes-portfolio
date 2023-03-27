1da8d8fd4a8f99b736a573aa8ca2348b77630f17
Matthew Starcher <mlstarcher@gmail.com>
2023-03-26 20:40:39 -0700

    added images to project, updated links

--- src/pages/CaseStudy1.js
+++ src/pages/CaseStudy1.js
@@ -1,20 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from 'react-icons/fa';
 import coming_soon from '../assets/coming_soon.svg';
 
 export default function CaseStudy1() {
   return (
     <div className="bg-offWhite">
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap sm:mb-32 mb-16 mt-8">
-        <div className="flex flex-col justify-center mb-8 sm:mb-0">
-          <h1 className="font-bold md:text-6xl text-2xl pb-4 text-offBlack">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap sm:mb-16 mb-16 mt-8">
+        <div className="flex flex-col justify-center items-center mb-8 sm:mb-0">
+          <h1 className="font-bold text-3xl sm:text-5xl text-offBlack pb-4">
             Cleverist
           </h1>
-          <p className="sm:text-2xl text-base">
+          <p className="text-base text-center px-8">
             A website that helps parents and caregivers find educational
             activities to do with their children at home.
           </p>
+          <Link
+            to={
+              'https://www.figma.com/file/Ezh6ISzgp5C1WpG1js5Myz/Cleverist-Prototypes?node-id=0%3A1&t=BzMBsCR7T6rMBXQM-1'
+            }
+            className="font-semibold text-accent hover:text-secondary text-sm"
+          >
+            <div className="flex items-center mt-4">
+              <p className="mr-2">View Prototype</p>
+              <FaArrowRight className="m-h-5" />
+            </div>
+          </Link>
         </div>
-        <div className="flex justify-center items-center mb-16 mt-8 sm:mb-0">
+        <div className="flex justify-center items-center mb-16">
           <div>
             <img
               src={coming_soon}
@@ -364,6 +377,317 @@ export default function CaseStudy1() {
             ></img>
           </div>
         </div>
+      </div>
+      <div className="flex flex-col mt-8 px-24 rounded-2xl bg-primary">
+        <h3 className="text-3xl text-center font-semibold mt-8">Design</h3>
+        <p className="mt-4 text-start text-sm">
+          After determining my information architecture, i got to work creating
+          concept sketches and worked on branding using Cleverist’s core values.
+          Using the sketches and branding, I moved on to wireframes to develop a
+          more complete design. I tried to focus on how the user interface could
+          be most time efficient for our user, while still providing a
+          <strong>delightful and inspiring experience.</strong>
+        </p>
+        <h2 className="text-2xl text-start font-semibold mt-16">
+          Early Sketches
+        </h2>
+        <p className="mt-4 text-start text-sm">
+          I began with some <strong>low fidelity</strong> concept sketches to
+          get an idea of how things would look. While these were a great place
+          to start, there was still a lot of UI work to do to pull it together.
+          In these early sketches, the focus was seeing how the flows would be
+          applied to the mobile site. I wanted to see where each step of the
+          flow would take the use. Starting with the navigation (hamburger
+          menu), choosing activities, then interest and finally art. From there
+          I laid out how the filtered activities would appear to the user, and
+          how an activity page would look once one activity was chosen.
+        </p>
+        <div className="flex justify-center items-center mb-16 mt-8 sm:mb-0">
+          <div>
+            <img
+              src={coming_soon}
+              className="max-w-full h-48"
+              alt="Portrait"
+            ></img>
+          </div>
+        </div>
+        <h2 className="text-2xl text-start font-semibold mt-16">Branding</h2>
+        <p className="mt-4 text-start text-sm">
+          With a general idea of how the site could be laid out, the next piece
+          was deciding how I would communicate the Cleverist values using
+          inspiration, color pallette, and logo. I chose to use colors that were
+          a playful spin on primary colors so often seen in children’s products.
+          The core values that I wanted to communicate with the overall branding
+          were:
+          <ul className="list-disc pl-4">
+            <li>Inspiring</li>
+            <li>Playful</li>
+            <li>Creative</li>
+            <li>Enriching</li>
+            <li>Approachable</li>
+          </ul>
+        </p>
+        <div className="flex justify-center items-center mb-16 mt-8 sm:mb-0">
+          <div>
+            <img
+              src={coming_soon}
+              className="max-w-full h-48"
+              alt="Portrait"
+            ></img>
+          </div>
+        </div>
+        <h2 className="text-2xl text-start font-semibold mt-16">
+          My Fidelity Wireframes
+        </h2>
+        <p className="mt-4 text-start text-sm">
+          Based on the sketches, I created mid fidelity,{' '}
+          <strong>responsive wireframes </strong> across mobile and desktop so
+          that users could access the site on different devices and have a
+          consistent experience. I like to think of these as the blueprint
+          stage. One main detail that was built apon was how the filters would
+          be applied, at this stage in the design I chose to makethe filters in
+          a checklist style rather than a braod drop down category so that the
+          user could choose multiple filter options.
+        </p>
+        <div className="flex justify-center items-center mb-16 mt-8 sm:mb-0">
+          <div>
+            <img
+              src={coming_soon}
+              className="max-w-full h-48"
+              alt="Portrait"
+            ></img>
+          </div>
+        </div>
+        <h2 className="text-2xl text-start font-semibold mt-16">
+          High Fidelity UI
+        </h2>
+        <p className="mt-4 text-start text-sm">
+          This step is where the design really came together visually. I was
+          able to fully design the Cleverist site while keeping our persona,
+          Megan in mind. A combination of custom made icons and real imagery
+          gave the site the exact feel we were going for in a way that was
+          simple, clean, and time efficient.
+        </p>
+        <Link
+          to={
+            'https://www.figma.com/file/jqZaAtwzFzrSrt6Mb1nqRA/Cleverist-Wireframes?node-id=1%3A3&t=05AJXlKrrTlhUCzR-1'
+          }
+          className="font-semibold text-accent hover:text-secondary text-sm"
+        >
+          <div className="flex items-center mt-4">
+            <p className="mr-2">View all designs in Figma</p>
+            <FaArrowRight className="m-h-5" />
+          </div>
+        </Link>
+        <div className="flex justify-center items-center mb-16 mt-8 sm:mb-0">
+          <div>
+            <img
+              src={coming_soon}
+              className="max-w-full h-48"
+              alt="Portrait"
+            ></img>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col mt-16 mb-8 px-24">
+        <h3 className="text-3xl text-center font-semibold">Test</h3>
+        <p className="mt-4 text-start text-sm">
+          At the end of the visual design portion came one of the most important
+          parts of the Cleverist design process - testing. I felt confident
+          about my overall design but conducting usability tests using the Figma
+          prototype would give clarity and inform my further iterations.
+        </p>
+        <Link
+          to={
+            'https://www.figma.com/file/Ezh6ISzgp5C1WpG1js5Myz/Cleverist-Prototypes?node-id=0%3A1&t=BzMBsCR7T6rMBXQM-1'
+          }
+          className="font-semibold text-accent hover:text-secondary text-sm"
+        >
+          <div className="flex items-center mt-4">
+            <p className="mr-2">View Prototype</p>
+            <FaArrowRight className="m-h-5" />
+          </div>
+        </Link>
+        <h2 className="text-2xl text-start font-semibold mt-16">
+          Usability Testing
+        </h2>
+        <p className="mt-4 text-start text-sm">
+          I conducted usability testing sessions to test the homepage, the
+          process of creating an account, and the process of finding and saving
+          an activity using filters. I held usability testing sessions with
+          three participants and documented all feedback.
+        </p>
+        <p className="mt-4 text-start text-sm">
+          The usability test goals were:
+          <ul className="list-disc pl-4">
+            <li>
+              To evaluate wether the prototype allows users to quickly and
+              effectively complete the task flows that the wireframes were
+              created for.
+            </li>
+            <li>
+              To determine if there were design issues that required iteration.
+            </li>
+          </ul>
+        </p>
+        <p className="mt-4 text-start text-sm">
+          The usability <strong>tests were considered successful</strong>{' '}
+          because all of the users could quickly and effectively complete the
+          task flows that the wireframes were created for. Some feedback was
+          given for future iterations which will be applied to make the
+          Cleverist site even easier and more delightful to use.
+        </p>
+        <div className="flex justify-center items-center mb-16 mt-8 sm:mb-0">
+          <div>
+            <img
+              src={coming_soon}
+              className="max-w-full h-48"
+              alt="Portrait"
+            ></img>
+          </div>
+        </div>
+        <h2 className="text-2xl text-start font-semibold mt-16">
+          Analyzing & Prioritizing Results
+        </h2>
+        <p className="mt-4 text-start text-sm">
+          I took the <strong>insights</strong> from the usability testing
+          sessions and organized them in a way that would show what worked, what
+          could change, what questions the participants had, and their
+          additional ideas. This organization helped me visualize{' '}
+          <strong>design opportunities</strong> and{' '}
+          <strong>potential next steps.</strong>
+        </p>
+        <div className="grid grid-cols-2 mt-8">
+          <div className="col-span-1">
+            <p className="font-semibold text-sm text-left">
+              What Worked:
+              <ul className="list-disc pl-4 mt-4 pr-4">
+                <li>Familiar and easy to navigate.</li>
+                <li>
+                  Colors, illustrations, typeface, and other UI were clean and
+                  minimal while remaining playful and interesting.
+                </li>
+                <li>Tasks were intuitive and easy to complete.</li>
+                <li>Information provided was detailed and comprehensive.</li>
+              </ul>
+            </p>
+          </div>
+          <div className="col-span-1">
+            <p className="font-semibold text-sm text-left">
+              What Could Change:
+              <ul className="list-disc pl-4 mt-4">
+                <li>Users felt there should be a button to apply filters.</li>
+                <li>
+                  Users wanted a heart or save icon on activities to allow
+                  activities to be saved to library.
+                </li>
+                <li>
+                  Users wanted to see an area where they could leave a comment
+                  on an activity.
+                </li>
+              </ul>
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-center items-center mb-16 mt-8 sm:mb-0">
+          <div>
+            <img
+              src={coming_soon}
+              className="max-w-full h-48"
+              alt="Portrait"
+            ></img>
+          </div>
+        </div>
+        <h2 className="text-2xl text-start font-semibold mt-16">
+          Final Iterations
+        </h2>
+        <p className="mt-4 text-start text-sm">
+          Based on the test results, I made some priority revisions throughout
+          the site based on how impactful it would be and how it would fit into
+          the time frame I had. Below are the main revisions that were made.
+        </p>
+        <ul className="list-disc pl-4 font-semibold text-sm text-left">
+          <li className="mt-4">
+            Button at the end of filter section to allow the user to apply
+            filters rather than clicking “X”.
+            <div className="flex justify-center items-center mb-16 mt-8 sm:mb-0">
+              <div>
+                <img
+                  src={coming_soon}
+                  className="max-w-full h-48"
+                  alt="Portrait"
+                ></img>
+              </div>
+            </div>
+          </li>
+          <li className="mt-4">
+            Button at the end of filter section to allow the user to apply
+            filters rather than clicking “X”.
+            <div className="flex justify-center items-center mb-16 mt-8 sm:mb-0">
+              <div>
+                <img
+                  src={coming_soon}
+                  className="max-w-full h-48"
+                  alt="Portrait"
+                ></img>
+              </div>
+            </div>
+          </li>
+          <li className="mt-4">
+            On the Activity page, “Leave a Comment” section added before the
+            existing comments from other users.
+            <div className="flex justify-center items-center mb-16 mt-8 sm:mb-0">
+              <div>
+                <img
+                  src={coming_soon}
+                  className="max-w-full h-48"
+                  alt="Portrait"
+                ></img>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className="flex flex-col mt-16 px-24 rounded-2xl bg-secondary">
+        <h3 className="text-3xl text-center font-semibold mt-8">Next Steps</h3>
+        <p className="mt-4 text-start text-sm mb-8">
+          With more time, the next steps I would take on this design project
+          would be:
+          <ul className="list-disc pl-4 font-semibold">
+            <li>Creating an “add your own activity” feature.</li>
+            <li>Design features for the Community section.</li>
+            <li>Add a FAQ section.</li>
+            <li>Continue to test and iterate.</li>
+          </ul>
+        </p>
+      </div>
+      <div className="flex flex-col mt-16 px-24 rounded-2xl bg-accent mb-16 text-white">
+        <h3 className="text-3xl text-center font-semibold mt-8">
+          Learning & Final Thoughts
+        </h3>
+        <p className="mt-4 text-left text-sm">
+          This was my first real UI/UX design project that I have worked on and
+          I learned so much from it.My main growth came in the areas of tool
+          sets (primarily Figma), and seeing a project from start to finish.
+        </p>
+        <p className="mt-4 text-left text-sm">
+          I also learned how I would like to continue to improve as a designer,.
+          After experiencing the entire design thinking process, I have a much
+          better idea how to organize projects and at which stages to seek
+          critique and feedback.
+        </p>
+        <p className="mt-4 text-left text-sm">
+          This was certainly an accelerated version of the Ui/UX design process,
+          and there are some things I would have pursued further if given more
+          time. I would have liked to see how the further iterations of my
+          design took shape, and how they would evolve through prototyping and
+          testing.
+        </p>
+        <p className="mt-4 text-left text-sm mb-8">
+          Although this project was new and sometimes challenging, I truly
+          enjoyed the challenge and feel that I will continue to stay curious
+          about the process and grow as a designer.
+        </p>
       </div>
     </div>
   );
