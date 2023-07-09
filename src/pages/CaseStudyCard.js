7c92af55f2d4d13226d4a0c7a05c7a49098149ca
Matthew Lee <mlee@Matthews-MacBook-Air.local>
2023-07-08 18:34:35 -0700

    Completed card refactor, added button component

--- src/pages/CaseStudyCard.js
+++ src/pages/CaseStudyCard.js
@@ -4,15 +4,15 @@ import ButtonPrimary from './ButtonPrimary.js';
 export default function CaseStudyCard({ case_study, index }) {
   const isOddIndex = index % 2 === 0;
   return (
-    <div className="grid grid-cols-12 sm:grid-cols-9 md:mb-40 mb-20 drop-shadow-md bg-white sm:py-16 py-8">
-      <div className={"pr-4 flex col-span-3 " + (isOddIndex ? 'md:order-1 md:col-start-2 md:col-end-6' : 'md:order-2 md:col-start-6 md:col-end-10')}>
+    <div className="flex flex-col md:flex-row md:mb-40 mb-20 drop-shadow-md bg-white sm:py-16 py-10 md:px-12 px-10">
+      <div className={"flex flex-col items-center justify-center w-full md:w-1/2 " + (isOddIndex ? 'md:order-first' : 'md:order-last')}>
         <img
           src={case_study.image_name}
           className="w-full h-auto object-contain"
           alt={case_study.alt_tag}
         ></img>
       </div>
-      <div className={"pr-4 flex flex-col items-start col-span-3 " + (!isOddIndex ? 'md:order-1 md:col-start-2 md:col-end-6' : 'md:order-2 md:col-start-6 md:col-end-10')}>
+      <div className={"flex flex-col items-start w-full md:w-1/2  " + (!isOddIndex ? 'md:order-first' : 'md:order-last')}>
         <h4 className="font-semibold text-2xl text-left md:text-4xl mb-6 md:mb-12">
           {case_study.title}
         </h4>
