77e0ab6582eb51f7435ab2d8f85bdffc9b19e26d
Matthew Lee <mlee@Matthews-MacBook-Air.local>
2023-07-11 20:56:16 -0700

    Adjusted welcome image size, updated landing page case study card images

--- src/data/caseStudyData.js
+++ src/data/caseStudyData.js
@@ -6,16 +6,7 @@ const caseStudyData = [
     short_description:
       'An app that helps users place orders for produce and goods from vendors at their local farmer’s market.',
     link: `local-basket`,
-    image_name: 'https://s3.us-west-2.amazonaws.com/brookestarcher.com/images/local_basket/iphone14.png',
-    alt_tag: 'iphones'
-  },
-  {
-    title: 'Cleverist',
-    sub_title: 'UI/UX DESIGN, BRANDING',
-    short_description:
-      'A responsive website where parents and caregivers can find educational activities to do with their children at home.',
-    link: `cleverist`,
-    image_name: 'https://s3.us-west-2.amazonaws.com/brookestarcher.com/images/cleverist/iPhone_14.png',
+    image_name: 'https://s3.us-west-2.amazonaws.com/brookestarcher.com/images/local_basket/lb_homepage_preview.png',
     alt_tag: 'iphones'
   },
   {
@@ -24,7 +15,7 @@ const caseStudyData = [
     short_description:
       'A responsive website where parents and caregivers can find educational activities to do with their children at home.',
     link: `sephora`,
-    image_name: 'https://s3.us-west-2.amazonaws.com/brookestarcher.com/images/sephora/macbook_air.png',
+    image_name: 'https://s3.us-west-2.amazonaws.com/brookestarcher.com/images/sephora/sephora_homepage_preview.png',
     alt_tag: 'iphones'
   },
   {
@@ -33,9 +24,18 @@ const caseStudyData = [
     short_description:
       'A responsive website where parents and caregivers can find educational activities to do with their children at home.',
     link: `guilder`,
-    image_name: 'https://s3.us-west-2.amazonaws.com/brookestarcher.com/images/guilder/iphone_14.png',
+    image_name: 'https://s3.us-west-2.amazonaws.com/brookestarcher.com/images/guilder/guilder_homepage_preview.png',
     alt_tag: 'iphones'
-  }
+  },
+  {
+    title: 'Cleverist',
+    sub_title: 'UI/UX DESIGN, BRANDING',
+    short_description:
+      'A responsive website where parents and caregivers can find educational activities to do with their children at home.',
+    link: `cleverist`,
+    image_name: 'https://s3.us-west-2.amazonaws.com/brookestarcher.com/images/cleverist/cleverist_homepage_preview.png',
+    alt_tag: 'iphones'
+  },
 ];
 
 export default caseStudyData;
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Welcome() {
   return (
       <div className="grid grid-cols-12 sm:mt-32 sm:mb-44 mt-8 mb-8">
-        <div className="col-span-12 md:col-start-4 md:col-end-9">
+        <div className="col-span-12 md:col-start-4 md:col-end-10">
           <img
             src={'https://s3.us-west-2.amazonaws.com/brookestarcher.com/images/global/welcome.png'}
             className="max-w-full sm:h-auto "
